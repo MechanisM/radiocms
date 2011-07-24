b6759d08d8b56595d5cd176004d1424063e4b0a1
Sam Starling <sam.starling@gmail.com>
2011-07-24 18:39:01 +0100

    Ran convert_to_south on music and programmes apps

---
+++ apps/music/migrations/0001_initial.py
@@ -0,0 +1,50 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Artist'
+        db.create_table('music_artist', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal('music', ['Artist'])
+
+        # Adding model 'Song'
+        db.create_table('music_song', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('artist', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['music.Artist'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal('music', ['Song'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Artist'
+        db.delete_table('music_artist')
+
+        # Deleting model 'Song'
+        db.delete_table('music_song')
+
+
+    models = {
+        'music.artist': {
+            'Meta': {'object_name': 'Artist'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'music.song': {
+            'Meta': {'object_name': 'Song'},
+            'artist': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['music.Artist']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['music']
