bd27d1e7db6b42f63c4942f4856c7fd3683fbd4f
Sam Starling <sam.starling@gmail.com>
2011-07-24 23:02:36 +0100

    * More class-based view wildness
    * Some rejigging of templates
    
    * /programmes/schedule/ now shows today's instances

--- apps/programmes/urls.py
+++ apps/programmes/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
-from views import EpisodeListView, EpisodeDetailView
+from views import EpisodeListView, EpisodeDetailView, InstanceTodayArchiveView
 
 urlpatterns = patterns('',
     url(r'^$', EpisodeListView.as_view(), name='home'),
-    url(r'^(?P<pk>\d+)/$', EpisodeDetailView.as_view(), name="episode_detail"),
+    url(r'^(?P<pk>\d+)/$', EpisodeDetailView.as_view(), name='episode_detail'),
+    url(r'^schedule/$', InstanceTodayArchiveView.as_view(), name='schedule_today')
 )
--- apps/programmes/views.py
+++ apps/programmes/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import DetailView, ListView
-from models import Episode
+from django.views.generic import DetailView, ListView, TodayArchiveView
+from models import Episode, Instance
 
 
 class EpisodeListView(ListView):
@@ -12,3 +12,10 @@ class EpisodeDetailView(DetailView):
 
     model = Episode
     context_object_name = 'episode'
+
+
+class InstanceTodayArchiveView(TodayArchiveView):
+
+    model = Instance
+    context_object_name = 'todays_instances_list'
+    date_field = 'tx_time'
